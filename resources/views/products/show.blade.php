83a968a7fa7df6279f911fb48af513b91cca11a7
ShokoMatsuda <syokom.0128@gmail.com>
2024-01-17 22:18:31 +0900

    23.4 フォームリクエストを使ってみよう

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel基礎</title>
-</head>
-<body>
-    <h1>{{ $vendor->vendor_name }}の商品一覧</h1>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>商品名</th>
-            <th>価格</th>
-            <th>作成日時</th>
-            <th>更新日時</th>
-        </tr>
-        @foreach($products as $product)
-        <tr>
-            <td>{{ $product->id }}</td>
-            <td>{{ $product->product_name }}</td>
-            <td>{{ $product->price }}</td>
-            <td>{{ $product->creatted_at }}</td>
-            <td>{{ $product->updated_at}}</td>
-        </tr>
-        @endforeach
-    </table>
-</body>
-</html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Laravel基礎</title>
+ </head>
+ 
+ <body>
+     <table>
+         <tr>
+             <th>ID</th>
+             <td>{{ $product->id }}</td>
+         </tr>
+         <tr>
+             <th>商品名</th>
+             <td>{{ $product->product_name }}</td>
+         </tr>
+         <tr>
+             <th>価格</th>
+             <td>{{ $product->price }}</td>
+         </tr>    
+         <tr>
+             <th>作成日時</th>
+             <td>{{ $product->created_at }}</td>            
+         </tr>
+         <tr>
+             <th>更新日時</th>
+             <td>{{ $product->updated_at }}</td>
+         </tr>      
+     </table>
+ </body>
+ 
+ </html>
